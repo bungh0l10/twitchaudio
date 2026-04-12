b169594f9c4b6603c5f2cf8db3450eada52368c2
bungh0l10 <maigatter@posteo.de>
2026-04-12 10:29:19 +0200

    cache

--- Plugin.pm
+++ Plugin.pm
@@ -4,9 +4,12 @@ use strict;
 use warnings;
 
 use base qw(Slim::Plugin::OPMLBased);
+
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
-use Slim::Utils::Strings qw(string  cstring);
+use Slim::Utils::Strings qw(cstring);
+use Slim::Utils::Cache;
+
 use Plugins::Twitch::API;
 
 my $prefs = preferences('plugin.twitch');
@@ -19,83 +22,135 @@ my $log = Slim::Utils::Log->addLogCategory({
 });
 
 sub getDisplayName {
-    return 'PLUGIN_TWITCH_NAME'
+    return 'PLUGIN_TWITCH_NAME';
 }
 
 sub initPlugin {
-    my $class = shift;
-    $log->debug("Initializing Twitch plugin");
+    my ($class) = @_;
 
     $class->SUPER::initPlugin(
         feed   => \&handleFeed,
         tag    => 'twitch',
         menu   => 'radios',
         is_app => 1,
-        weight  => 1
+        weight => 1
     );
 
     Slim::Player::ProtocolHandlers->registerHandler(
         twitch => 'Plugins::Twitch::ProtocolHandler'
     );
+
+    return;
 }
 
 sub handleFeed {
     my ($client, $cb, $args) = @_;
-    $log->debug("handleFeed called");
 
-    my $items = [
-        { name => cstring($client, 'PLUGIN_TWITCH_SEARCH'), type => 'search', url => \&searchChannel },
-    ];
+    my $items = _buildRootItems($client);
 
     $cb->({ items => $items });
+
+    return;
 }
 
 sub searchChannel {
     my ($client, $cb, $args) = @_;
 
-    my $search = $args->{search} || '';
-    $search =~ s/^\s+|\s+$//g;
-    $search = lc $search;
+    my $query = _normalizeQuery($args->{search});
 
-    $log->info("Search query: $search");
-
-    Plugins::Twitch::API::getChannel($search, sub {
+    Plugins::Twitch::API::getChannel($query, sub {
         my ($data) = @_;
-        my $user = $data->{user} // undef;
 
-        # Kanal existiert nicht
-        unless ($user) {
-            return $cb->({
-                items => [{
-                    name => cstring($client, 'PLUGIN_TWITCH_CHANNEL_DOES_NOT_EXIST'),
-                    type => 'link',
-                }]
-            });
-        }
+        return _handleNoUser($client, $cb)
+            unless $data && $data->{user};
 
-        my $stream = $user->{stream} // undef;
-        my $title  = $stream ? $stream->{title} : 'Offline';
-        my $cover  = $user->{profileImageURL} || '';
-        my $artist = $user->{login};
+        my $item = _buildChannelItem($data->{user});
 
-        my $url = $stream ? Plugins::Twitch::API::getAudioUrl($artist) : undef;
+        _cacheMetadata($item->{artist}, $item);
 
-        my $items = [
-            {
-                type           => 'audio',
-                favorites_type => 'audio',
-                play           => 'twitch://' . $artist,
-                name           => $artist,
-                line1          => $artist,
-                line2          => $title,
-                image          => $cover,
-                on_select      => 'play',
-                duration       => 0,
-            }
-        ];
+        $cb->({ items => [$item] });
 
-        $cb->({ items => $items });
+        return;
     });
+
+    return;
+}
+
+sub _buildRootItems {
+    my ($client) = @_;
+
+    return [
+        {
+            name => cstring($client, 'PLUGIN_TWITCH_SEARCH'),
+            type => 'search',
+            url  => \&searchChannel
+        }
+    ];
+}
+
+sub _handleNoUser {
+    my ($client, $cb) = @_;
+
+    $cb->({
+        items => [{
+            name => cstring($client, 'PLUGIN_TWITCH_CHANNEL_DOES_NOT_EXIST'),
+            type => 'link',
+        }]
+    });
+
+    return;
+}
+
+sub _buildChannelItem {
+    my ($user) = @_;
+
+    my $stream = $user->{stream} || {};
+
+    my $artist = lc($user->{login} || '');
+    my $title  = $stream->{title} || 'Offline';
+    my $cover  = $user->{profileImageURL} || '';
+
+    return {
+        type           => 'audio',
+        favorites_type => 'audio',
+        play           => 'twitch://' . $artist,
+        name           => $artist,
+        line1          => $artist,
+        line2          => $title,
+        image          => $cover,
+        on_select      => 'play',
+        duration       => 0,
+        artist         => $artist,
+    };
+}
+
+sub _cacheMetadata {
+    my ($artist, $item) = @_;
+
+    return unless $artist;
+
+    my $cache = Slim::Utils::Cache->new;
+
+    $cache->set(
+        "twitch_meta_$artist",
+        {
+            title  => $item->{line2},
+            artist => $artist,
+            cover  => $item->{image},
+        },
+        60
+    );
+
+    return;
+}
+
+sub _normalizeQuery {
+    my ($query) = @_;
+
+    return '' unless defined $query;
+
+    $query =~ s/^\s+|\s+$//g;
+    return lc $query;
 }
 
 1;
--- ProtocolHandler.pm
+++ ProtocolHandler.pm
@@ -8,71 +8,114 @@ use parent qw(Slim::Player::Protocols::HTTPS);
 use Slim::Utils::Log qw(logger);
 use Slim::Utils::Scanner::Remote ();
 use Slim::Control::Request ();
+use Slim::Utils::Cache;
+use Slim::Player::Client;
 
 use Plugins::Twitch::API ();
 
 my $log = logger('plugin.twitch');
 
-
 sub scanUrl {
     my ($class, $uri, $args) = @_;
 
-    return unless defined $uri && defined $args;
+    return unless $uri && $args;
 
-    my $channel = _channelFromUri($uri);
-    return unless defined $channel;
+    my $channel = _extractChannel($uri);
+    return unless $channel;
 
-    my $client = $args->{client};
-    return unless defined $client;
+    my $client = _resolveClient($args->{client});
+    return unless $client;
 
-    Plugins::Twitch::API::getChannel(
-        $channel,
-        sub {
-            my ($data) = @_;
-            return unless defined $data;
+    my $stream_url = Plugins::Twitch::API::getAudioUrl($channel);
+    return unless $stream_url;
 
-            my $user   = $data->{user}   || {};
-            my $stream = $user->{stream} || {};
+    Slim::Utils::Scanner::Remote->scanURL($stream_url, $args);
 
-            my $url = Plugins::Twitch::API::getAudioUrl($channel);
-            return unless defined $url;
+    my $cache = Slim::Utils::Cache->new;
+    my $cached_meta = $cache->get("twitch_meta_$channel");
 
-            Slim::Utils::Scanner::Remote->scanURL($url, $args);
+    _applyMetadata($client, $channel, $cached_meta);
 
-            my $song = $client->playingSong;
-            return unless defined $song;
+    return;
+}
 
-            my $cover = $user->{profileImageURL} || q{};
+sub _applyMetadata {
+    my ($client, $channel, $cached_meta) = @_;
 
-            my $meta = {
-                title  => $stream->{title},
-                artist => $user->{login},
-                cover  => $cover,
-                icon   => $cover,
-                name   => q{},
-            };
+    return unless $client;
 
-            $song->pluginData({ wmaMeta => $meta });
+    my $song = eval { $client->playingSong };
+    return unless $song;
 
-            Slim::Control::Request::notifyFromArray(
-                $client,
-                ['newmetadata'],
-            );
+    if ($cached_meta) {
+        _setSongMetadata($client, $song, $cached_meta);
+        return;
+    }
 
-            return;
-        },
+    Plugins::Twitch::API::getChannel($channel, sub {
+        my ($data) = @_;
+        return unless $data;
+
+        my $meta = _buildMetadata($channel, $data);
+
+        my $current_song = eval { $client->playingSong };
+        return unless $current_song;
+
+        _setSongMetadata($client, $current_song, $meta);
+
+        my $cache = Slim::Utils::Cache->new;
+        $cache->set("twitch_meta_$channel", $meta, 3600);
+
+        return;
+    });
+
+    return;
+}
+
+sub _setSongMetadata {
+    my ($client, $song, $meta) = @_;
+
+    return unless $song;
+
+    $song->pluginData({ wmaMeta => $meta });
+
+    Slim::Control::Request::notifyFromArray(
+        $client,
+        ['newmetadata']
     );
 
     return;
 }
 
+sub _buildMetadata {
+    my ($channel, $data) = @_;
 
-sub _channelFromUri {
+    my $user   = $data->{user}   || {};
+    my $stream = $user->{stream} || {};
+
+    return {
+        title  => $stream->{title} || 'Offline',
+        artist => $user->{login}   || $channel,
+        cover  => $user->{profileImageURL} || '',
+    };
+}
+
+sub _resolveClient {
+    my ($client) = @_;
+
+    return unless $client;
+
+    return $client if ref $client;
+
+    return Slim::Player::Client::getClient($client);
+}
+
+sub _extractChannel {
     my ($uri) = @_;
 
-    return unless defined $uri;
+    return unless $uri;
 
-    if ( $uri =~ m{^twitch://([^/]+)} ) {
+    if ($uri =~ m{^twitch://([^/]+)}) {
         return lc $1;
     }
 
